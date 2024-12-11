d0468b179e56d8b326570f498d672e5c5d7e1d3a
Hannah Apuan <hannah.apuan@gmail.com>
2024-12-11 12:18:28 -0700

    day 8: reading in input file, parsing values to map and antennas

--- 08/main.go
+++ 08/main.go
@@ -4,36 +4,60 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 )
 
 const (
 	filename    string = "example.txt"
 	openVal     rune   = '.'
 	antinodeVal rune   = '#'
+	regex       string = "[a-zA-Z0-9]"
 )
 
 type cell struct {
-	val  rune
-	x, y int
+	val       rune
+	x, y      int
+	isAntenna bool
+	// matchingAntennas []cell
 }
 
+// type antennaPair struct {
+// 	a1 cell
+// 	a2 cell
+// }
+
 func main() {
+	m, atl, err := readInput(filename)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	fmt.Println(mapToString(m))
+	fmt.Println(atl)
 
 }
 
 // Reads the input file and initializes the grid and guard's starting state
-func readInput(fname string) ([][]cell, error) {
+func readInput(fname string) ([][]cell, map[rune][]cell, error) {
+	antennaToLocation := make(map[rune][]cell)
+
 	cells := make([][]cell, 0) // 2D array representing the grid
 
 	file, err := os.Open(fname)
 	if err != nil {
-		return nil, fmt.Errorf("error opening file:  %w", err)
+		return nil, nil, fmt.Errorf("error opening file:  %w", err)
 	}
 	defer file.Close()
 
 	reader := bufio.NewReader(file)
 	var row []cell
 	var i, j int
+
+	// regex checks if the character is a-z, A-Z, 0-9
+	r, err := regexp.Compile(regex)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error compiling regex string: %s", err)
+	}
 
 	for {
 		char, _, err := reader.ReadRune()
@@ -44,7 +68,7 @@ func readInput(fname string) ([][]cell, error) {
 				}
 				break
 			}
-			return nil, fmt.Errorf("error reading file: %w", err)
+			return nil, nil, fmt.Errorf("error reading file: %w", err)
 		}
 
 		if char == '\n' {
@@ -56,9 +80,33 @@ func readInput(fname string) ([][]cell, error) {
 			continue
 		}
 
+		// add cell to grid
 		currCell := cell{x: i, y: j, val: char}
+
+		// antenna case
+		if r.MatchString(string(char)) {
+			currCell.isAntenna = true
+			_, ok := antennaToLocation[char]
+			if !ok {
+				antennaToLocation[char] = make([]cell, 0)
+			}
+			antennaToLocation[char] = append(antennaToLocation[char], currCell)
+		}
+
 		row = append(row, currCell)
 		i++
 	}
-	return cells, nil
+	return cells, antennaToLocation, nil
+}
+
+// Converts the grid into a printable string representation
+func mapToString(s [][]cell) string {
+	var export string
+	for _, row := range s {
+		for _, cell := range row {
+			export += fmt.Sprintf("%s ", string(cell.val))
+		}
+		export += "\n"
+	}
+	return export
 }
